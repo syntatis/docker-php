4a69d163263e1596f3776805918947f759428b66
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-01-10 00:21:26 +0700

    Update `entrypoint.sh` scripts

--- wp/php8.1/entrypoint.sh
+++ wp/php8.1/entrypoint.sh
@@ -2,12 +2,12 @@
 
 if [ wp core version > /dev/null 2>&1 ]; then
   echo "WordPress is already downloaded.";
-  wp core verify-checksums;
+  wp core verify-checksums --allow-root;
 else
   echo "WordPress is not yet downloaded.";
   if [ "$WP_CORE_DOWNLOAD" = "true" ]; then
     echo "Downloading WordPress.";
     wp core download --force --version=${WP_VERSION:-latest} --locale=${WP_LOCALE:-en_US} --allow-root;
-    wp core verify-checksums;
+    wp core verify-checksums --allow-root;
   fi
 fi
